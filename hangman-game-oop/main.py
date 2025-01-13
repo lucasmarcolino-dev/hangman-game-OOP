4a925cbd845636c8038d1e3712173bc569f6f576
Lucas <lucas.aroucha@actionsys.com.br>
2025-01-13 16:01:35 -0300

    Ajustes fluxo game

--- hangman-game-oop/main.py
+++ hangman-game-oop/main.py
@@ -88,6 +88,9 @@ class Hangman:
     def show_board(self):
         print(board[self.errors])
 
+    def show_answers(self):
+        print('Letras que já foram: ', self.answers, end=' ')
+
     def check_letter(self, letter):
         if not (letter in self.answers):
             self.answers.append(letter)
@@ -95,27 +98,30 @@ class Hangman:
                 for i in range(len(self.list_word)):
                     if self.list_word[i] == letter:
                         self.list_underscore[i] = letter
-                print(board[self.errors])
-                self.show_underscore()
             else:
                 self.errors += 1
-                print(board[self.errors])
-                self.show_underscore()
                 if self.errors == 6:
+                    self.show_board()
                     print()
                     print('Você perdeu!')
                     print('A palavra era: ' + self.word)
         else:
             print('Letra já inserida! Insira outra letra')
-
 if __name__ == '__main__':
     print('>>>>>>>>>>Hangman<<<<<<<<<<')
 
     player1 = Hangman()
 
     while player1.errors < 6:
-        player1.show_board()
 
-        player1.show_underscore()
+        if player1.list_underscore != player1.list_word:
+            player1.show_answers()
 
-        player1.check_letter(input('\n\nInsira a letra: '))
+            player1.show_board()
+
+            player1.show_underscore()
+
+            player1.check_letter(input('\n\nInsira a letra: '))
+        else:
+            print('Você acertou!')
+            break
